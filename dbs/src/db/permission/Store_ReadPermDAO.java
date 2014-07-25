133e1495b5a649621ebfae1f78be55d0dd3b6e74
Patrick Kann <pkkann@gmail.com>
2014-07-25 14:13:57 +0200

    Store_ReadPermDAO finished untested

---
+++ dbs/src/db/permission/Store_ReadPermDAO.java
@@ -0,0 +1,102 @@
+
+package db.permission;
+
+import abstracts.PermissionDAO;
+import abstracts.Register;
+import db.DBTool;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.data.Store;
+import model.data.User;
+import model.data.UserRegister;
+import model.permission.Store_ReadPerm;
+
+/**
+ *
+ * @author Patrick
+ */
+public class Store_ReadPermDAO extends PermissionDAO<Store, Store_ReadPerm> {
+
+    public Store_ReadPermDAO(UserRegister userRegister, Register<Store> register) {
+        super(userRegister, register);
+        table = "read_store_perm";
+    }
+
+    @Override
+    public void insert(Store_ReadPerm source) {
+        try {
+            Statement st = DBTool.getStatement();
+            
+            int targetStoreId = source.getTargetStore().getId();
+            int executorUserId = source.getExecutorUser().getId();
+            
+            String sql = "INSERT INTO "+table+" ('target_store_id', 'executor_user_id') VALUES("+targetStoreId+", "+executorUserId+");";
+            st.execute(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Store_ReadPerm source) {
+        try {
+            Statement st = DBTool.getStatement();
+            
+            int targetStoreId = source.getTargetStore().getId();
+            int executorUserId = source.getExecutorUser().getId();
+            
+            String sql = "DELETE FROM "+table+" WHERE target_store_id="+targetStoreId+" AND executor_user_id="+executorUserId+";";
+            st.execute(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public Store_ReadPerm select(User executor, Store target) {
+        try {
+            Statement st = DBTool.getStatement();
+            
+            int targetStoreId = target.getId();
+            int executorUserId = executor.getId();
+            
+            String sql = "SELECT * FROM "+table+" WHERE target_store_id="+targetStoreId+" AND executor_user_id="+executorUserId+";";
+            try (ResultSet rs = st.executeQuery(sql)) {
+                while(rs.next()) {
+                    Store_ReadPerm store_ReadPerm = new Store_ReadPerm(target, executor);
+                    return store_ReadPerm;
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<Store_ReadPerm> select(User executor) {
+        ArrayList<Store_ReadPerm> store_ReadPerms = new ArrayList<>();
+        try {
+            Statement st = DBTool.getStatement();
+            
+            int executorUserId = executor.getId();
+            
+            String sql = "SELECT * FROM "+table+" WHERE executor_user_id="+executorUserId+";";
+            try (ResultSet rs = st.executeQuery(sql)) {
+                while(rs.next()) {
+                    int targetStoreId = rs.getInt("target_store_id");
+                    Store target = register.get(targetStoreId);
+                    Store_ReadPerm store_ReadPerm = new Store_ReadPerm(target, executor);
+                    store_ReadPerms.add(store_ReadPerm);
+                }
+            }
+            return store_ReadPerms;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+}
