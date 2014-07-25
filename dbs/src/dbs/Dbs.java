1f82033ff3ef7d421cab3d0c7dfc9448419e5b76
Patrick Kann <pkkann@gmail.com>
2014-07-25 16:11:49 +0200

    DBS changes

--- dbs/src/dbs/Dbs.java
+++ dbs/src/dbs/Dbs.java
@@ -1,79 +1,69 @@
 package dbs;
 
-import db.data.DealerDAO;
-import db.data.ProductDAO;
-import db.data.SaleDAO;
-import db.data.StoreDAO;
-import db.data.TagDAO;
-import db.data.UserDAO;
-import db.permission.AdminPermDAO;
-import db.permission.Dealer_AdminPermDAO;
-import db.permission.Dealer_CreatePermDAO;
-import db.permission.Dealer_DeletePermDAO;
-import db.permission.Dealer_ReadPermDAO;
-import db.permission.Dealer_UpdatePermDAO;
-import model.data.DealerRegister;
-import model.data.ProductRegister;
-import model.data.SaleRegister;
-import model.data.StoreRegister;
-import model.data.TagRegister;
-import model.data.UserRegister;
+import model.data.*;
+import model.permission.*;
+import db.data.*;
+import db.permission.*;
 
 /**
  *
  * @author Patrick
  */
 public class Dbs {
+    
+    private DealerDAO dealerDAO;
+    private DealerRegister dealerRegister;
+    
+    private StoreDAO storeDAO;
+    private StoreRegister storeRegister;
+    
+    private ProductDAO productDAO;
+    private ProductRegister produdtRegister;
+    
+    private SaleDAO saleDAO;
+    private SaleRegister saleRegister;
+    
+    private UserDAO userDAO;
+    private UserRegister userRegister;
+    
+    private TagDAO tagDAO;
+    private TagRegister tagRegister;
 
-    private final DealerDAO dealerDAO;
-    private final DealerRegister dealerRegister;
-    private final StoreDAO storeDAO;
-    private final StoreRegister storeRegister;
-    private final UserDAO userDAO;
-    private final UserRegister userRegister;
-    private final ProductDAO productDAO;
-    private final ProductRegister productRegister;
-    private final SaleDAO saleDAO;
-    private final SaleRegister saleRegister;
-    private final TagDAO tagDAO;
-    private final TagRegister tagRegister;
-
-    private final AdminPermDAO adminPermDAO;
-    private final Dealer_AdminPermDAO dealer_AdminPermDAO;
-    private final Dealer_CreatePermDAO dealer_CreatePermDAO;
-    private final Dealer_ReadPermDAO dealer_ReadPermDAO;
-    private final Dealer_UpdatePermDAO dealer_UpdatePermDAO;
-    private final Dealer_DeletePermDAO dealer_DeletePermDAO;
+    private AdminPermDAO adminPermDAO;
+    
+    private Dealer_AdminPermDAO dealer_AdminPermDAO;
+    private Dealer_CreatePermDAO dealer_CreatePermDAO;
+    private Dealer_DeletePermDAO dealer_DeletePermDAO;
+    private Dealer_ReadPermDAO dealer_ReadPermDAO;
+    private Dealer_UpdatePermDAO dealer_UpdatePermDAO;
+    
+    private Product_CreatePermDAO product_CreatePermDAO;
+    private Product_DeletePermDAO product_DeletePermDAO;
+    private Product_ReadPermDAO product_ReadPermDAO;
+    private Product_UpdatePermDAO product_UpdatePermDAO;
+    
+    private Sale_CreatePermDAO sale_CreatePermDAO;
+    private Sale_DeletePermDAO sale_DeletePermDAO;
+    private Sale_ReadPermDAO sale_ReadPermDAO;
+    private Sale_UpdatePermDAO sale_UpdatePermDAO;
+    
+    private Store_AdminPermDAO store_AdminPermDAO;
+    private Store_CreatePermDAO store_CreatePermDAO;
+    private Store_DeletePermDAO store_DeletePermDAO;
+    private Store_ReadPermDAO store_ReadPermDAO;
+    private Store_UpdatePermDAO store_UpdatePermDAO;
+    
+    private  User_CreatePermDAO user_CreatePermDAO;
+    private  User_DeletePermDAO user_DeletePermDAO;
+    private  User_ReadPermDAO user_ReadPermDAO;
+    private  User_UpdatePermDAO user_UpdatePermDAO;
 
     public Dbs() {
-        this.dealerDAO = new DealerDAO();
-        this.dealerRegister = new DealerRegister(dealerDAO);
-        this.storeDAO = new StoreDAO(dealerRegister);
-        this.storeRegister = new StoreRegister(storeDAO);
-        this.userDAO = new UserDAO(dealerRegister, storeRegister);
-        this.userRegister = new UserRegister(userDAO);
-        this.productDAO = new ProductDAO(dealerRegister, storeRegister);
-        this.productRegister = new ProductRegister(productDAO);
-        this.saleDAO = new SaleDAO(dealerRegister, storeRegister);
-        this.saleRegister = new SaleRegister(saleDAO);
-        this.tagDAO = new TagDAO();
-        this.tagRegister = new TagRegister(tagDAO);
         
-        this.adminPermDAO = new AdminPermDAO(userRegister, null);
-        this.dealer_AdminPermDAO = new Dealer_AdminPermDAO(userRegister, dealerRegister);
-        this.dealer_CreatePermDAO = new Dealer_CreatePermDAO(userRegister, null);
-        this.dealer_ReadPermDAO = new Dealer_ReadPermDAO(userRegister, dealerRegister);
-        this.dealer_UpdatePermDAO = new Dealer_UpdatePermDAO(userRegister, dealerRegister);
-        this.dealer_DeletePermDAO = new Dealer_DeletePermDAO(userRegister, dealerRegister);
     }
 
     public void load() {
-        dealerRegister.load();
-        storeRegister.load();
-        userRegister.load();
-        productRegister.load();
-        saleRegister.load();
-        tagRegister.load();
+        
     }
 
     /**
