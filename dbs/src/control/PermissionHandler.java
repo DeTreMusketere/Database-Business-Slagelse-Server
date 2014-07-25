4f695287fe06f7a2b6e841e4a7e42ac9db00db13
Patrick Kann <pkkann@gmail.com>
2014-07-25 16:11:42 +0200

    PermissionHandler on its way

--- dbs/src/control/PermissionHandler.java
+++ dbs/src/control/PermissionHandler.java
@@ -2,32 +2,10 @@
 package control;
 
 import db.DBTool;
+import db.permission.*;
 import java.util.ArrayList;
 import model.data.User;
-import model.permission.UserPermissionSet;
-import model.permission.AdminPerm;
-import model.permission.Dealer_AdminPerm;
-import model.permission.Dealer_CreatePerm;
-import model.permission.Dealer_DeletePerm;
-import model.permission.Dealer_ReadPerm;
-import model.permission.Dealer_UpdatePerm;
-import model.permission.Product_CreatePerm;
-import model.permission.Product_DeletePerm;
-import model.permission.Product_ReadPerm;
-import model.permission.Product_UpdatePerm;
-import model.permission.Sale_CreatePerm;
-import model.permission.Sale_DeletePerm;
-import model.permission.Sale_ReadPerm;
-import model.permission.Sale_UpdatePerm;
-import model.permission.Store_AdminPerm;
-import model.permission.Store_CreatePerm;
-import model.permission.Store_DeletePerm;
-import model.permission.Store_ReadPerm;
-import model.permission.Store_UpdatePerm;
-import model.permission.User_CreatePerm;
-import model.permission.User_DeletePerm;
-import model.permission.User_ReadPerm;
-import model.permission.User_UpdatePerm;
+import model.permission.*;
 
 /**
  *
@@ -35,9 +13,62 @@ import model.permission.User_UpdatePerm;
  */
 public class PermissionHandler {
     
-    public PermissionHandler() {
-        
+    private final AdminPermDAO adminPermDAO;
+    
+    private final Dealer_AdminPermDAO dealer_AdminPermDAO;
+    private final Dealer_CreatePermDAO dealer_CreatePermDAO;
+    private final Dealer_DeletePermDAO dealer_DeletePermDAO;
+    private final Dealer_ReadPermDAO dealer_ReadPermDAO;
+    private final Dealer_UpdatePermDAO dealer_UpdatePermDAO;
+    
+    private final Product_CreatePermDAO product_CreatePermDAO;
+    private final Product_DeletePermDAO product_DeletePermDAO;
+    private final Product_ReadPermDAO product_ReadPermDAO;
+    private final Product_UpdatePermDAO product_UpdatePermDAO;
+    
+    private final Sale_CreatePermDAO sale_CreatePermDAO;
+    private final Sale_DeletePermDAO sale_DeletePermDAO;
+    private final Sale_ReadPermDAO sale_ReadPermDAO;
+    private final Sale_UpdatePermDAO sale_UpdatePermDAO;
+    
+    private final Store_AdminPermDAO store_AdminPermDAO;
+    private final Store_CreatePermDAO store_CreatePermDAO;
+    private final Store_DeletePermDAO store_DeletePermDAO;
+    private final Store_ReadPermDAO store_ReadPermDAO;
+    private final Store_UpdatePermDAO store_UpdatePermDAO;
+    
+    private final User_CreatePermDAO user_CreatePermDAO;
+    private final User_DeletePermDAO user_DeletePermDAO;
+    private final User_ReadPermDAO user_ReadPermDAO;
+    private final User_UpdatePermDAO user_UpdatePermDAO;
+
+    public PermissionHandler(AdminPermDAO adminPermDAO, Dealer_AdminPermDAO dealer_AdminPermDAO, Dealer_CreatePermDAO dealer_CreatePermDAO, Dealer_DeletePermDAO dealer_DeletePermDAO, Dealer_ReadPermDAO dealer_ReadPermDAO, Dealer_UpdatePermDAO dealer_UpdatePermDAO, Product_CreatePermDAO product_CreatePermDAO, Product_DeletePermDAO product_DeletePermDAO, Product_ReadPermDAO product_ReadPermDAO, Product_UpdatePermDAO product_UpdatePermDAO, Sale_CreatePermDAO sale_CreatePermDAO, Sale_DeletePermDAO sale_DeletePermDAO, Sale_ReadPermDAO sale_ReadPermDAO, Sale_UpdatePermDAO sale_UpdatePermDAO, Store_AdminPermDAO store_AdminPermDAO, Store_CreatePermDAO store_CreatePermDAO, Store_DeletePermDAO store_DeletePermDAO, Store_ReadPermDAO store_ReadPermDAO, Store_UpdatePermDAO store_UpdatePermDAO, User_CreatePermDAO user_CreatePermDAO, User_DeletePermDAO user_DeletePermDAO, User_ReadPermDAO user_ReadPermDAO, User_UpdatePermDAO user_UpdatePermDAO) {
+        this.adminPermDAO = adminPermDAO;
+        this.dealer_AdminPermDAO = dealer_AdminPermDAO;
+        this.dealer_CreatePermDAO = dealer_CreatePermDAO;
+        this.dealer_DeletePermDAO = dealer_DeletePermDAO;
+        this.dealer_ReadPermDAO = dealer_ReadPermDAO;
+        this.dealer_UpdatePermDAO = dealer_UpdatePermDAO;
+        this.product_CreatePermDAO = product_CreatePermDAO;
+        this.product_DeletePermDAO = product_DeletePermDAO;
+        this.product_ReadPermDAO = product_ReadPermDAO;
+        this.product_UpdatePermDAO = product_UpdatePermDAO;
+        this.sale_CreatePermDAO = sale_CreatePermDAO;
+        this.sale_DeletePermDAO = sale_DeletePermDAO;
+        this.sale_ReadPermDAO = sale_ReadPermDAO;
+        this.sale_UpdatePermDAO = sale_UpdatePermDAO;
+        this.store_AdminPermDAO = store_AdminPermDAO;
+        this.store_CreatePermDAO = store_CreatePermDAO;
+        this.store_DeletePermDAO = store_DeletePermDAO;
+        this.store_ReadPermDAO = store_ReadPermDAO;
+        this.store_UpdatePermDAO = store_UpdatePermDAO;
+        this.user_CreatePermDAO = user_CreatePermDAO;
+        this.user_DeletePermDAO = user_DeletePermDAO;
+        this.user_ReadPermDAO = user_ReadPermDAO;
+        this.user_UpdatePermDAO = user_UpdatePermDAO;
     }
+    
+    
     
     public UserPermissionSet constructUserPermissionSet(User u) {
         UserPermissionSet ups = new UserPermissionSet();
@@ -46,36 +77,36 @@ public class PermissionHandler {
         AdminPerm adminperm;
         
         //Dealer
-        ArrayList<Dealer_AdminPerm> dealer_AdminPerm;
-        Dealer_CreatePerm dealer_CreatePerm;
-        ArrayList<Dealer_ReadPerm> dealer_ReadPerms;
-        ArrayList<Dealer_UpdatePerm> dealer_UpdatePerms;
-        ArrayList<Dealer_DeletePerm> dealer_DeletePerms;
+        ArrayList<Dealer_AdminPerm> dealer_AdminPerm = dealer_AdminPermDAO.select(u);
+        Dealer_CreatePerm dealer_CreatePerm = dealer_CreatePermDAO.select2(u);
+        ArrayList<Dealer_ReadPerm> dealer_ReadPerms = dealer_ReadPermDAO.select(u);
+        ArrayList<Dealer_UpdatePerm> dealer_UpdatePerms = dealer_UpdatePermDAO.select(u);
+        ArrayList<Dealer_DeletePerm> dealer_DeletePerms = dealer_DeletePermDAO.select(u);
         
         //Store
-        ArrayList<Store_AdminPerm> store_AdminPerms;
-        ArrayList<Store_CreatePerm> store_CreatePerms;
-        ArrayList<Store_ReadPerm> store_ReadPerms;
-        ArrayList<Store_UpdatePerm> store_UpdatePerms;
-        ArrayList<Store_DeletePerm> store_DeletePerms;
+        ArrayList<Store_AdminPerm> store_AdminPerms = store_AdminPermDAO.select(u);
+        ArrayList<Store_CreatePerm> store_CreatePerms = store_CreatePermDAO.select(u);
+        ArrayList<Store_ReadPerm> store_ReadPerms = store_ReadPermDAO.select(u);
+        ArrayList<Store_UpdatePerm> store_UpdatePerms = store_UpdatePermDAO.select(u);
+        ArrayList<Store_DeletePerm> store_DeletePerms = store_DeletePermDAO.select(u);
         
         //User
-        ArrayList<User_CreatePerm> user_CreatePerms;
-        ArrayList<User_ReadPerm> user_ReadPerms;
-        ArrayList<User_UpdatePerm> user_UpdatePerms;
-        ArrayList<User_DeletePerm> user_DeletePerms;
+        ArrayList<User_CreatePerm> user_CreatePerms = user_CreatePermDAO.select(u);
+        ArrayList<User_ReadPerm> user_ReadPerms = user_ReadPermDAO.select(u);
+        ArrayList<User_UpdatePerm> user_UpdatePerms = user_UpdatePermDAO.select(u);
+        ArrayList<User_DeletePerm> user_DeletePerms = user_DeletePermDAO.select(u);
         
         //product
         ArrayList<Product_CreatePerm> product_CreatePerms;
-        ArrayList<Product_ReadPerm> product_ReadPerms;
-        ArrayList<Product_UpdatePerm> product_UpdatePerms;
-        ArrayList<Product_DeletePerm> product_DeletePerms;
+        ArrayList<Product_ReadPerm> product_ReadPerms = product_ReadPermDAO.select(u);
+        ArrayList<Product_UpdatePerm> product_UpdatePerms = product_UpdatePermDAO.select(u);
+        ArrayList<Product_DeletePerm> product_DeletePerms = product_DeletePermDAO.select(u);
         
         //Sale
         ArrayList<Sale_CreatePerm> sale_CreatePerms;
-        ArrayList<Sale_ReadPerm> sale_ReadPerms;
-        ArrayList<Sale_UpdatePerm> Sale_UpdatePerms;
-        ArrayList<Sale_DeletePerm> sale_DeletePerms;
+        ArrayList<Sale_ReadPerm> sale_ReadPerms = sale_ReadPermDAO.select(u);
+        ArrayList<Sale_UpdatePerm> sale_UpdatePerms = sale_UpdatePermDAO.select(u);
+        ArrayList<Sale_DeletePerm> sale_DeletePerms = sale_DeletePermDAO.select(u);
         
         
         DBTool.close();
