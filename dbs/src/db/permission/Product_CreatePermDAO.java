83b6d8f2aa0f281d7cf803520937f57a19b9b926
Patrick Kann Station <pkkann@live.dk>
2014-07-28 11:18:20 +0200

    Product_CreatePermDAO fixed

--- dbs/src/db/permission/Product_CreatePermDAO.java
+++ dbs/src/db/permission/Product_CreatePermDAO.java
@@ -10,8 +10,10 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.data.Dealer;
+import model.data.DealerRegister;
 import model.data.Product;
 import model.data.Store;
+import model.data.StoreRegister;
 import model.data.User;
 import model.data.UserRegister;
 import model.permission.Product_CreatePerm;
@@ -21,9 +23,14 @@ import model.permission.Product_CreatePerm;
  * @author Patrick
  */
 public class Product_CreatePermDAO extends PermissionDAO<Product, Product_CreatePerm> {
+    
+    private DealerRegister dealerRegister;
+    private StoreRegister storeRegister;
 
-    public Product_CreatePermDAO(UserRegister userRegister, Register<Product> register) {
+    public Product_CreatePermDAO(UserRegister userRegister, Register<Product> register, DealerRegister dealerRegister, StoreRegister storeRegister) {
         super(userRegister, register);
+        this.dealerRegister = dealerRegister;
+        this.storeRegister = storeRegister;
         table = "create_product_perm";
     }
 
@@ -105,6 +112,39 @@ public class Product_CreatePermDAO extends PermissionDAO<Product, Product_Create
         return null;
     }
     
+    @Override
+    public ArrayList<Product_CreatePerm> select(User executor) {
+        ArrayList<Product_CreatePerm> product_CreatePerms = new ArrayList<>();
+        try {
+            Statement st = DBTool.getStatement();
+            
+            int executorUserId = executor.getId();
+            
+            String sql = "SELECT * FROM "+table+" WHERE executor_user_id="+executorUserId+";";
+            try (ResultSet rs = st.executeQuery(sql)) {
+                while(rs.next()) {
+                    int parentDealerId = rs.getInt("parent_dealer_id");
+                    int parentStoreId = rs.getInt("parent_store_id");
+                    
+                    if(parentStoreId != 0) {
+                        Store parentStore = storeRegister.get(parentStoreId);
+                        Product_CreatePerm product_CreatePerm = new Product_CreatePerm(executor, parentStore);
+                        product_CreatePerms.add(product_CreatePerm);
+                    } else {
+                        Dealer parentDealer = dealerRegister.get(parentDealerId);
+                        Product_CreatePerm product_CreatePerm = new Product_CreatePerm(executor, parentDealer);
+                        product_CreatePerms.add(product_CreatePerm);
+                    }
+                    
+                }
+            }
+            return product_CreatePerms;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
     public Product_CreatePerm select2(User executor, Store parent) {
         try {
             Statement st = DBTool.getStatement();
@@ -124,11 +164,6 @@ public class Product_CreatePermDAO extends PermissionDAO<Product, Product_Create
             ex.printStackTrace();
         }
         return null;
-    }
-
-    @Override
-    public ArrayList<Product_CreatePerm> select(User executor) {
-        throw new UnsupportedOperationException("Method not supported");
     }
 
 }
