cd09bbcfb3f6960c96f14c2a510431c232395fb5
Patrick Kann <pkkann@gmail.com>
2014-07-29 17:21:39 +0200

    Added version and title

--- dbs/src/dbs/Dbs.java
+++ dbs/src/dbs/Dbs.java
@@ -12,6 +12,9 @@ import db.permission.*;
  */
 public class Dbs {
     
+    public static final String TITLE = "DBS";
+    public static final String VERSION = "0.0.0.0";
+    
     private DealerDAO dealerDAO;
     private DealerRegister dealerRegister;
     
