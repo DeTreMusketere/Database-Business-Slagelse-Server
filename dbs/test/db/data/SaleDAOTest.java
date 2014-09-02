4fae96048a96f8e28778429da8f34ad44ed9a918
Astrinaar <7h380x@gmail.com>
2014-09-02 08:44:25 +0200

    data JUnit tests all work

--- dbs/test/db/data/SaleDAOTest.java
+++ dbs/test/db/data/SaleDAOTest.java
@@ -23,18 +23,18 @@ import static org.junit.Assert.*;
  */
 public class SaleDAOTest extends InstanceTests {
 
-    private static Dealer parentDealer;
-    private static Sale testSale;
-    private static int idDealer;
-    private static Picture picture1;
-    private static Picture picture2;
+    private Dealer parentDealer;
+    private Dealer expectedResultParentDealer;
+    private Sale testSale;
+    private Picture picture1;
+    private Picture picture2;
 
     public SaleDAOTest() {
     }
 
     @BeforeClass
     public static void setUpClass() {
-        
+
     }
 
     @AfterClass
@@ -45,30 +45,23 @@ public class SaleDAOTest extends InstanceTests {
     public void setUp() {
         String name = "Billige bananer";
         String description = "Super billige Chickadicka bananer fra Sverige";
-        
-        picture1 = new Picture(0, "Fugl", "");
-        int pictureId = pictureDAO.insert(picture1);
-        picture1.setId(pictureId);
-        
+        picture1 = pictureRegister.create("test", "data/pictures/test.png");
         double price = 10.50;
-        parentDealer = new Dealer(0, "Fakta", "Det tager kun 5 minutter", "2342523525", picture1);
-        idDealer = dealerRegister.insert(parentDealer);
-        parentDealer.setId(idDealer);
+        parentDealer = dealerRegister.create("Fakta", "Det tager kun 5 minutter, hvis køen ikke er for lang", "12346587", null);
+        parentDealer.setId(parentDealer.getId());
         Date start = new Date(2000, 03, 21, 12, 00);
         Date end = new Date(2000, 03, 21, 15, 00);
         Date publish = new Date(2000, 03, 21, 12, 00);
-        testSale = new Sale(0, name, description, picture1, price, start, end, publish, parentDealer);
-        int idSale = saleDAO.insert(testSale);
-        testSale.setId(idSale);
-        
-        pictureRegister.load();
-        
+        testSale = saleRegister.create(name, description, picture1, price, start, end, publish, parentDealer);
     }
 
     @After
     public void tearDown() {
-        saleDAO.delete(testSale);
-        pictureDAO.delete(picture1);
+        saleRegister.delete(testSale);
+        dealerRegister.delete(parentDealer);
+        dealerRegister.delete(expectedResultParentDealer);
+        pictureRegister.delete(picture1);
+        pictureRegister.delete(picture2);
     }
 
     /**
@@ -82,30 +75,24 @@ public class SaleDAOTest extends InstanceTests {
         int expectedResultId = testSale.getId();
         String expectedResultName = "Chickadicka bananer";
         String expectedResultDescription = "Super billige Chickadicka bananer fra Sverige";
-        
-        
-        picture2 = new Picture(0, "Fisk", "");
-        int pictureId = pictureDAO.insert(picture2);
-        picture2.setId(pictureId);
-        
+
+        picture2 = pictureRegister.create("test2", "data/pictures/test2.png");
+
         Date start = new Date(2001, 03, 21, 12, 00);
         Date end = new Date(2001, 03, 21, 15, 00);
         Date publish = new Date(2001, 03, 21, 12, 00);
         long expectedResultStart = start.getTime();
         long expectedResultEnd = end.getTime();
         long expectedResultPublish = publish.getTime();
-        
+
         String expectedResultPictureString = picture2.getName();
         double expectedResultPrice = 8.50;
-        Dealer expectedResultParentDealer = parentDealer;
-        Sale source = new Sale(expectedResultId, expectedResultName, expectedResultDescription, picture2, expectedResultPrice, start, end, publish, expectedResultParentDealer);
+        expectedResultParentDealer = dealerRegister.create("Føtex", "Vores butik er større end din", "87546132", null);
+        Sale source = new Sale(testSale.getId(), expectedResultName, expectedResultDescription, picture2, expectedResultPrice, start, end, publish, expectedResultParentDealer, testSale.getUpdateNumber());
 
-        pictureRegister.load();
-        
-        saleDAO.update(source, target);
-        
-        Sale resultSale = saleDAO.select(testSale.getId());
-        
+        saleRegister.update(source, target);
+
+        Sale resultSale = saleRegister.get(source.getId());
 
         int resultId = resultSale.getId();
         String resultName = resultSale.getName();
@@ -116,8 +103,6 @@ public class SaleDAOTest extends InstanceTests {
         long resultStart = resultSale.getStart().getTime();
         long resultEnd = resultSale.getEnd().getTime();
         long resultPublish = resultSale.getPublish().getTime();
-        
-        
 
         assertEquals(expectedResultId, resultId);
         assertEquals(expectedResultName, resultName);
@@ -128,8 +113,7 @@ public class SaleDAOTest extends InstanceTests {
         assertEquals(expectedResultStart, resultStart);
         assertEquals(expectedResultEnd, resultEnd);
         assertEquals(expectedResultPublish, resultPublish);
-        
-        pictureDAO.delete(picture2);
+
     }
 
 }
