40ea0fad741f5be79a62b9f8511e8de356109b99
Knappmann <7h380x@live.dk>
2014-09-04 16:11:39 +0200

    capitalizeWords og capitalizeSentences added

--- dbs/src/control/StringTool.java
+++ dbs/src/control/StringTool.java
@@ -1,4 +1,3 @@
-
 package control;
 
 /**
@@ -6,21 +5,52 @@ package control;
  * @author Patrick
  */
 public class StringTool {
-    
+
     /**
      * Converts a string to replace æ, ø and å with unicode values
+     *
      * @param string The string to convert.
      * @return Converted string.
      */
-    public static String convertÆØÅ(String string){
+    public static String convertÆØÅ(String string) {
         String s1 = string.replaceAll("æ", "\u00E6");
         String s2 = s1.replaceAll("Æ", "\u00C6");
         String s3 = s2.replaceAll("ø", "\u00F8");
         String s4 = s3.replaceAll("Ø", "\u00D8");
         String s5 = s4.replaceAll("å", "\u00E5");
         String s6 = s5.replaceAll("Å", "\u00C5");
-        
+
         return string;
+    }
+
+    public static String capitalizeWords(String string) {
+        String resultString = "";
+        String[] split = string.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            String s = split[i];
+            resultString += s.substring(0, 1).toUpperCase() + s.substring(1);
+            if (i + 1 < split.length) {
+                resultString += " ";
+            }
+        }
+        return resultString;
+    }
+
+    public static String capitalizeSentences(String string) {
+        String resultString = "";
+        String[] split = string.split("\\Q. \\E");
+        for (int i = 0; i < split.length; i++) {
+            String s = split[i];
+            System.out.println(split[i]);
+            resultString += s.substring(0, 1).toUpperCase() + s.substring(1);
+            if (i + 1 < split.length) {
+                resultString += ". ";
+            }
+        }
+        if (resultString.charAt(resultString.length() - 1) != '.') {
+            resultString += ".";
+        }
+        return resultString;
     }
 
 }
